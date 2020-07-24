8850c4d35b3e3beb8284ac1de70d2de1e2b231d4
Yath Seanghay <seanghay.dev@gmail.com>
2020-07-24 17:22:26 +0700

    initial commit

---
+++ binding.js
@@ -0,0 +1,37 @@
+const { spawn, exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const executableFilePath = path.join(__dirname, 'pgp-starbucks.jar');
+
+
+function encryptAsString(publicKeyPath, content) {
+    return new Promise((resolve, reject) => {
+
+        if (!publicKeyPath || !content) {
+            reject('public key path and data are required');
+            return;
+        }
+
+        const result = spawn('java', ['-jar', executableFilePath, publicKeyPath, content]);
+        const chunks = [];
+
+
+        result.stdout.on('data', (data) => {
+            chunks.push(...data);
+        });
+        
+        result.stdout.on('end', () => {
+            const content = Buffer.from(chunks).toString();
+            resolve(content);
+        });
+
+        result.stderr.on('data', (err) => {
+            const msg = Buffer.from(err).toString();
+            reject(msg);
+        })
+    });
+}
+
+module.exports = {
+    encryptAsString
+}
---
+++ main.js
@@ -0,0 +1,26 @@
+const { encryptAsString } = require('./binding');
+
+module.exports.templateTags = [
+    {
+        name: 'pigy',
+        displayName: 'Pigy',
+        description: 'Generate PGP Bouncycastle',
+        args: [
+            {
+                displayName: 'Content',
+                description: 'Content to be encrypted',
+                type: 'string',
+                defaultValue: ''
+            },
+            {
+                displayName: 'Public Key',
+                description: 'A key use to encrypt the content',
+                type: 'file',
+            }
+        ],
+        async run(context, content, publicKey) {
+            console.log({ publicKey, content })
+            return await encryptAsString(publicKey, content);
+        }
+    }
+]
